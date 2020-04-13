245e01a24df3c87a219d8da248a39684ed16832e
cqy6260 <1261540220@qq.com>
2020-04-13 17:28:41 +0800

    作业5 陈勤业 03

---
+++ Study/hello.py
@@ -0,0 +1,16 @@
+
+def odd(start,end):
+    for i in range(start,end):
+        if i % 2 == 1:
+            print(i)
+
+def even(start,end):
+    for i in range(start, end):
+        if i % 2 == 0:
+            print(i)
+
+odd(1,101)
+even(1,101)
+
+
+
